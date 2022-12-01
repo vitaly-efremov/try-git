18f6753f3c27b7e6caaee114e296968c06febc79
v.efremov <v.efremov@cian.ru>
2022-12-01 18:46:25 +0700

    Add counter library

---
+++ counter.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+def counter(elements: list[str]) -> dict[str, int]:
+	counter = defaultdict(int)
+	for element in elements:
+		counter[element] += 1
+	return dict(counter)
+
+if __name__ == '__main__':
+	assert counter([1, 2, 1]) =={1: 2, 2: 1}
+	
