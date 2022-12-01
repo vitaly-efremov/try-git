a6f9b36ec0db587afeb7f24602391fda3552e3be
v.efremov <v.efremov@cian.ru>
2022-12-01 18:57:30 +0700

    Fix pep-8 and enhance typing

--- counter.py
+++ counter.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
-def counter(elements: list[str]) -> dict[str, int]:
+
+def count(elements: list[int]) -> dict[int, int]:
 	counter = defaultdict(int)
 	for element in elements:
 		counter[element] += 1
@@ -8,7 +9,6 @@ def counter(elements: list[str]) -> dict[str, int]:
 
 
 if __name__ == '__main__':
-	assert counter([1, 2, 1]) == {1: 2, 2: 1}
-	assert counter([]) == {}
-	assert counter([1, 2, 3]) == {1: 1, 2: 1, 3: 1}
-	
+	assert count([1, 2, 1]) == {1: 2, 2: 1}
+	assert count([]) == {}
+	assert count([1, 2, 3]) == {1: 1, 2: 1, 3: 1}
