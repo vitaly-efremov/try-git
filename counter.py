afd28e47dfab1153b1b812a47978a59e6a1c8e18
v.efremov <v.efremov@cian.ru>
2022-12-01 18:54:02 +0700

    Add tests and fix pep-8 issues

--- counter.py
+++ counter.py
@@ -6,6 +6,9 @@ def counter(elements: list[str]) -> dict[str, int]:
 		counter[element] += 1
 	return dict(counter)
 
+
 if __name__ == '__main__':
-	assert counter([1, 2, 1]) =={1: 2, 2: 1}
+	assert counter([1, 2, 1]) == {1: 2, 2: 1}
+	assert counter([]) == {}
+	assert counter([1, 2, 3]) == {1: 1, 2: 1, 3: 1}
 	
